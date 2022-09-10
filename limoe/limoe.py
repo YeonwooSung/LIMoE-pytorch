0ae0d134c503eb3b004bc9cce41788302c08d11d
YeonwooSung <neos960518@gmail.com>
2022-09-10 21:14:13 +0900

    Implement Text embedding layer

--- limoe/limoe.py
+++ limoe/limoe.py
@@ -108,13 +108,66 @@ class SelfAttention(nn.Module):
 #-------------------#
 # LIMoE Embedding
 
-class LIMoEEmbedding(nn.Module):
+class LIMoETextEmbedding(nn.Module):
     def __init__(self, config:LIMoEConfig) -> None:
         super().__init__()
-    
-    def forward(self, input_ids):
-        #TODO
-        pass
+
+        pad_token_id = getattr(config, "pad_token_id", 0)
+        hidden_size = config.hidden_size
+
+        # word embeddings
+        self.embedding_size = getattr(config, "embedding_size", hidden_size)
+        self.word_embeddings = nn.Embedding(config.vocab_size, self.embedding_size, padding_idx=pad_token_id)
+
+        # position embeddings
+        self.position_embeddings = nn.Embedding(config.max_position_embeddings, self.embedding_size)
+
+        # token type embeddings
+        type_vocab_size = getattr(config, "type_vocab_size", 2)
+        if type_vocab_size > 0:
+            self.token_type_embeddings = nn.Embedding(type_vocab_size, self.embedding_size)
+
+        # projection layer
+        if self.embedding_size != hidden_size:
+            self.embed_proj = nn.Linear(self.embedding_size, hidden_size, bias=False)
+        
+        self.LayerNorm = LIMoELayerNorm(hidden_size, config.layer_norm_eps)
+        self.dropout = StableDropout(config.hidden_dropout_prob)
+
+
+    def forward(self, input_ids=None, token_type_ids=None, position_ids=None, mask=None, inputs_embeds=None):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if position_ids is None:
+            position_ids = torch.arange(seq_length, dtype=torch.long, device=input_ids.device)
+            position_ids = position_ids.unsqueeze(0).expand(input_shape)
+        
+        if token_type_ids is None:
+            token_type_ids = torch.zeros(input_shape, dtype=torch.long, device=input_ids.device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embeddings(input_ids)
+
+        position_embeddings = self.position_embeddings(position_ids)
+        embeddings = inputs_embeds + position_embeddings
+
+        if hasattr(self, "token_type_embeddings"):
+            token_type_embeddings = self.token_type_embeddings(token_type_ids)
+            embeddings += token_type_embeddings
+
+        if hasattr(self, "embed_proj"):
+            embeddings = self.embed_proj(embeddings)
+
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+
+        return embeddings
+
 
 
 #-------------------#
@@ -247,7 +300,9 @@ class LIMoE(nn.Module):
         super().__init__()
         self.config = config
 
-        self.embeddings = LIMoEEmbedding(config)
+        self.embeddings = LIMoETextEmbedding(config)
+        # self.embeddings = LIMoEImageEmbedding(config)
+
         self.encoder = LIMoEEncoder(config)
 
         # TODO: add the output layer
