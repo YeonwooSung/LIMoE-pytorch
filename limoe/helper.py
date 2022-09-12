cb315dd78e6ef51e896ffe57856ac40c96824e32
YeonwooSung <neos960518@gmail.com>
2022-09-12 21:29:41 +0900

    Design LimoeOutput

--- limoe/helper.py
+++ limoe/helper.py
@@ -1,5 +1,7 @@
+from dataclasses import dataclass
 import torch
 import torch.nn as nn
+from typing import Optional, Tuple
 
 
 #-------------------#
@@ -99,3 +101,29 @@ class StableDropout(nn.Module):
             return ctx
         else:
             return self.drop_prob
+
+
+#-------------------#
+# Output
+
+@dataclass
+class LimoeModelOutput:
+    """
+    Base class for model's outputs, with potential hidden states and attentions.
+    Args:
+        last_hidden_state (`torch.FloatTensor` of shape `(batch_size, sequence_length, hidden_size)`):
+            Sequence of hidden-states at the output of the last layer of the model.
+        hidden_states (`tuple(torch.FloatTensor)`, *optional*, returned when `output_hidden_states=True` is passed or when `config.output_hidden_states=True`):
+            Tuple of `torch.FloatTensor` (one for the output of the embeddings, if the model has an embedding layer, +
+            one for the output of each layer) of shape `(batch_size, sequence_length, hidden_size)`.
+            Hidden-states of the model at the output of each layer plus the optional initial embedding outputs.
+        attentions (`tuple(torch.FloatTensor)`, *optional*, returned when `output_attentions=True` is passed or when `config.output_attentions=True`):
+            Tuple of `torch.FloatTensor` (one for each layer) of shape `(batch_size, num_heads, sequence_length,
+            sequence_length)`.
+            Attentions weights after the attention softmax, used to compute the weighted average in the self-attention
+            heads.
+    """
+
+    last_hidden_state: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
